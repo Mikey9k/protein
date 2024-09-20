4cfe7c4df29e9a3f7a54422d7d42c476424983b4
Mikey9k <michaelyzhengcs@gmail.com>
2024-09-20 07:37:31 +0000

    filters working

--- my-project/src/components/Header.jsx
+++ my-project/src/components/Header.jsx
@@ -3,17 +3,23 @@ import { GiGorilla } from "react-icons/gi";
 
 export default function Header() {
     return (
-        <div className="flex flex-col items-center">
-            <div className="flex items-center mt-4 mb-4">
-                <GiGorilla size={70} color='white' />
-                <span className="font-bold text-white text-4xl ml-2">ProteinBuddy</span>
+        <div>
+            <div className="flex flex-col items-center cursor-pointer" onClick={() => window.location.reload()}>
+                <div className="flex items-center mt-4 mb-4">
+                    <GiGorilla size={70} color='white' />
+                    <span className="font-bold text-white text-4xl ml-2">ProteinBuddy</span>
+                </div>
+         
             </div>
-            <hr className="w-full border-t-1 border-white mb-4" />
+            <div className="flex flex-col items-center">
 
-            <h2 className="text-xl md:text-2xl lg:text-3xl font-semibold text-white mb-4">
-                What type of protein are you looking for?
-            </h2>
-            
+                <hr className="w-full border-t-1 border-white mb-4" />
+
+                <h2 className="text-xl md:text-2xl lg:text-4xl font-semibold text-white mb-4">
+                    What type of protein are you looking for?
+                </h2>
+                
+            </div>
         </div>
     );
 }
--- my-project/src/components/List.jsx
+++ my-project/src/components/List.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 // import Breadcrumb from './Breadcrumb';
 // import ResultRow from './ResultRow';
 import axios from "axios";
-import { sortBy } from 'lodash';
+import _, { sortBy } from 'lodash';
 // import { data } from 'autoprefixer';
 // import LoadingSkeleton from './LoadingSkeleton';
 import { format, toZonedTime } from 'date-fns-tz';
@@ -12,11 +12,22 @@ import Cardv2 from './Cardv2';
 import './List.css';
 import CardTemplate from './CardTemplate';
 import { GiPassport } from 'react-icons/gi';
+import './SortedBar.css';
 
 export default function List({items}) {
     const [latest, setLatest] = useState("");
     const [cachedResults,setCachedResults] = useState([]);
     const [filteredResults, setFilteredResults] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [empty, setEmpty] = useState(false);
+    const [selectedOption1, setSelectedOption1] = useState('Lowest Unit Price');
+  
+
+    const handleSelect1 = (option) => {
+        setSelectedOption1(option);
+    };
+
+
 
     console.log(items);
     let filterString = "";
@@ -30,13 +41,19 @@ export default function List({items}) {
         filterString += `?flavour=${items[0]}&category=${items[1]}`;
     }
 
-    console.log(filterString);
+    // console.log(filterString);
 
     useEffect(() => {
+        setEmpty(false);
+        setLoading(true);
         axios.get(`https://proteinbuddy.onrender.com/record/products${filterString}`)
           .then(res => {
             setFilteredResults(res.data);
             console.log(res.data);
+            setLoading(false);
+            if (res.data === "No records found") {
+                setEmpty(true);
+            }
           });
     }, [filterString]);
 
@@ -45,7 +62,7 @@ export default function List({items}) {
           .then(res => {
 
             setLatest(res.data.updatedAt);
-            console.log(res.data.updatedAt);
+            // console.log(res.data.updatedAt);
           });
     }, []);
     
@@ -64,11 +81,31 @@ export default function List({items}) {
     // }, []);
 
     // const sortedCache = sortBy(cachedResults, "value").reverse();
-    const sortedFiltered = sortBy(filteredResults, "weight").reverse();
+    // console.log(filteredResults)
+    let sortedFiltered = [];
+    if (filteredResults !== "No records found") {
+
+        if (selectedOption1 === 'Lowest Unit Price') {
+            sortedFiltered = sortBy(filteredResults, "value");
+        } else if (selectedOption1 === 'Price (Low to high)') { 
+            sortedFiltered = sortBy(filteredResults, "currentPrice");
+        } else if (selectedOption1 === 'Price (High to low)') {
+            sortedFiltered = sortBy(filteredResults, "currentPrice").reverse();
+        } else {
+            sortedFiltered = sortBy(filteredResults, "weight").reverse();
+
+        }
+
+    } else {
+        // setEmpty(true);
+    }
+
+    console.log(empty);
 
     return (
         <div>
-            <div className="result-row p-4 rounded-lg block relative border border-white/10 shadow-lg mt-8">
+
+            <div className="result-row p-4 rounded-lg block relative border border-white/20 shadow-xl mt-8">
 
                       {/* <Card /> */}
                 {/* <div className="card-container">
@@ -82,52 +119,102 @@ export default function List({items}) {
                     <CardTemplate />
                     </div>
                 </div> */}
-
-
-                <div className="card-container">
-                    {sortedFiltered.slice(0, 3).map((result, index) => (
-                        <div key={result._id} className="flex items-center space-x-4">
-                            {/* <div className="w-8 flex justify-center items-center">
-                                {index === 0 && <span className="text-5xl">&#129351;</span>}
-                                {index === 1 && <span className="text-4xl">&#129352;</span>}
-                                {index === 2 && <span className="text-4xl">&#129353;</span>}
-                                {index > 2 && <span className="text-2xl font-bold">{index + 1}</span>}
-                            </div> */}
-                            <div className="card-item flex-1">
-                                <Cardv2
-                                    providername={result.title}
-                                    weight={result.weight}
-                                    price={result.currentPrice}
-                                    value={result.value}
-                                    logo={result.image}
-                                    link={result.url}
-                                />
-                            </div>
-                        </div>
-                    ))}
-                </div>
-
-
-                {sortedFiltered.slice(0, 10).map((result, index) => (
-                    <div key={result._id} className="flex items-center space-x-4">
-                        <div className="w-8 flex justify-center items-center">
-                            {index === 0 && <span className="text-5xl">&#129351;</span>}
-                            {index === 1 && <span className="text-4xl">&#129352;</span>}
-                            {index === 2 && <span className="text-4xl">&#129353;</span>}
-                            {index > 2 && <span className="text-2xl font-bold">{index + 1}</span>}
-                        </div>
-                        <div className="flex-1">
-                            <ResultRow
-                                providername={result.title}
-                                weight={result.weight}
-                                price={result.currentPrice}
-                                value={result.value}
-                                logo={result.image}
-                                link={result.url}
-                            />
+                <div className="filter-bar top-right">
+                    <div>Sort By:</div>
+                    <div className="dropdown-container">
+                        <div className="dropdown-display">{selectedOption1}</div>
+                        <div className="dropdown-menu">
+                            <div className="dropdown-item" onClick={() => handleSelect1('Lowest Unit Price')}>Lowest Unit Price</div>
+                            <div className="dropdown-item" onClick={() => handleSelect1('Price (Low to high)')}>Price (Low to high)</div>
+                            <div className="dropdown-item" onClick={() => handleSelect1('Price (High to low)')}>Price (High to low)</div>
                         </div>
                     </div>
-                ))}
+                </div>
+
+                <br></br>
+                <br></br>
+                <br></br>
+
+                {loading && (
+                    <div className="loading-indicator flex flex-col justify-center items-center space-y-2 mt-16">
+                        <div className="w-8 h-8 border-4 border-blue-500 border-solid border-t-transparent rounded-full animate-spin"></div>
+                        <p className="text-blue-500 font-medium">Loading...</p>
+                    </div>
+                )}
+
+
+                {empty && (
+                    <div className="flex flex-col justify-center items-center space-y-4">
+                        {/* Funny GIF */}
+                        <img 
+                        src="https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExeHQ3aXR6eDhtNXpsZG95dDJhNXVmdnZpNDJqMnQ2OGFuejYwMGV6dyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9dg/kfRPfxMScM8WGVPMJY/giphy.gif" 
+                        alt="Empty state funny gif" 
+                        className="w-64 h-64 object-cover rounded-lg shadow-lg" 
+                        />
+
+                        {/* Message */}
+                        <p className="text-lg text-gray-700 font-semibold">
+                        Oops! Looks like there's nothing here.
+                        </p>
+                        {/* <p className="text-gray-500">
+                        Maybe your data took a day off. 😄
+                        </p> */}
+                    </div>
+                )}
+
+
+
+
+                {!loading && (
+                    <div>
+                        <div className="card-container">
+                            {sortedFiltered.slice(0, 3).map((result, index) => (
+                                <div key={result._id} className="flex items-center space-x-4">
+                                    {/* <div className="w-8 flex justify-center items-center">
+                                        {index === 0 && <span className="text-5xl">&#129351;</span>}
+                                        {index === 1 && <span className="text-4xl">&#129352;</span>}
+                                        {index === 2 && <span className="text-4xl">&#129353;</span>}
+                                        {index > 2 && <span className="text-2xl font-bold">{index + 1}</span>}
+                                    </div> */}
+                                    <div className="card-item flex-1">
+                                        <Cardv2
+                                            providername={result.title}
+                                            weight={result.weight}
+                                            price={result.currentPrice}
+                                            value={result.value}
+                                            logo={result.image}
+                                            link={result.url}
+                                        />
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+
+
+                        {sortedFiltered.slice(0, 10).map((result, index) => (
+                            <div key={result._id} className="flex items-center space-x-4">
+                                <div className="w-8 flex justify-center items-center">
+                                    {index === 0 && <span className="text-5xl">&#129351;</span>}
+                                    {index === 1 && <span className="text-4xl">&#129352;</span>}
+                                    {index === 2 && <span className="text-4xl">&#129353;</span>}
+                                    {index > 2 && <span className="text-2xl font-bold">{index + 1}</span>}
+                                </div>
+                                <div className="flex-1">
+                                    <ResultRow
+                                        providername={result.title}
+                                        weight={result.weight}
+                                        price={result.currentPrice}
+                                        value={result.value}
+                                        logo={result.image}
+                                        link={result.url}
+                                    />
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                )}
+
+
             </div>
 
             <div className='mt-6'>
